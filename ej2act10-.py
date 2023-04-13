c8b3c912a406911ea11cd253f5bd7e76ddca4665
AleMM <ale.molmir@gmail.com>
2023-04-13 01:44:43 -0300

    modificado ej 10

--- ej2act10-.py
+++ ej2act10-.py
@@ -29,10 +29,12 @@ notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37
 
 # inciso1
 def generador_diccio(nombres, notas_1, notas_2):
-    # nombres = nombres.replace('\'','').replace(',','')
-    lista_nombres = "".join([" " if letter in "'," else letter for letter in nombres])
-    lista_nombres = lista_nombres.strip()
-    lista_nombres = lista_nombres.split()
+    # # nombres = nombres.replace('\'','').replace(',','')
+    # lista_nombres = "".join([" " if letter in "'," else letter for letter in nombres])
+    # lista_nombres = lista_nombres.strip()
+    # lista_nombres = lista_nombres.split()
+    lista_nombres = "".join([" " if letter in "'," else letter for letter in nombres]).strip().split()
+
     # print(len(lista_nombres))# print(len(notas_2))# print(lista_nombres)
     return {
         alumno: (nota_1, nota_2)
@@ -47,15 +49,15 @@ def calcular_prom_alumno(*args):
 
 
 def calcular_promedios_alumnos(registro_alumnos):
-    return {
-        alumno: (
-            calcular_prom_alumno(
-                registro_alumnos[alumno][0], registro_alumnos[alumno][1]
-            )
-        )
-        for alumno in registro_alumnos
-    }
-
+    # return {
+    #     alumno: (
+    #         calcular_prom_alumno(
+    #             registro_alumnos[alumno][0], registro_alumnos[alumno][1]
+    #         )
+    #     )
+    #     for alumno in registro_alumnos
+    # }
+    return dict(map(lambda alumno:(alumno[0], calcular_prom_alumno(*alumno[1])),registro_alumnos.items())) # desempaquetar con *
 
 # inciso3
 def calcular_promedio_total(promedios):
@@ -66,24 +68,27 @@ def calcular_promedio_total(promedios):
 # def identificar_alum_max_nota (alumnos_ y_notas):
 #     return max(alumnos_y_notas)
 def identificar_alum_max_nota(promedios):
-    valor_max = max(promedios.values())
-    indice = list(promedios.values()).index(valor_max)
-    return list(promedios.keys())[
-        indice
-    ]  # comprimido: list(promedios.keys())[list(promedios.values()).index(max(promedios.values()))]
-
+    # valor_max = max(promedios.values())
+    # indice = list(promedios.values()).index(valor_max)
+    # return list(promedios.keys())[
+    #     indice
+    # ]  # comprimido: list(promedios.keys())[list(promedios.values()).index(max(promedios.values()))
+    return max(promedios.items(), key = lambda alumno:alumno[1])[0]
 
 # inciso5
 def identificar_alum_min_nota(registro_alumnos):  # analizar con map
-    # minimos =[]
-    # for alumno in registro_alumnos:
-    #     minimos.append(min(registro_alumnos[alumno]))
-    minimos = [min(registro_alumnos[alumno]) for alumno in registro_alumnos]
-    valor_min = min(minimos)
-    indice = minimos.index(valor_min)
-    return list(registro_alumnos.keys())[
-        indice
-    ]  # comprimido: list(registro_alumnos.keys())[minimos.index(min(minimos))]
+    # # minimos =[]
+    # # for alumno in registro_alumnos:
+    # #     minimos.append(min(registro_alumnos[alumno]))
+    # minimos = [min(registro_alumnos[alumno]) for alumno in registro_alumnos]registro_alumnos
+    # valor_min = min(minimos)
+    # indice = minimos.index(valor_min)
+    # return list(registro_alumnos.keys())[
+    #     indice
+    # ]  # comprimido: list(registro_alumnos.keys())[minimos.index(min(minimos))]
+    return min(registro_alumnos.items(), key = lambda alumno: min(alumno[1]))[0]
+   # return min(list(map(lambda alumno: min(alumno[1]), registro_alumnos.items()))) #no sale el nombre
+
 
 
 # ---------------------------------------------------------------------------------------------------------------
@@ -105,7 +110,6 @@ print("")
 # inciso3 - Calcular el promedio general del curso.
 promedio_total = calcular_promedio_total(promedios)
 print("El promedio total de la clase es: ", promedio_total)
-2.0
 
 # inciso4 - Identificar al estudiante con la nota promedio más alta.
 alumno_max = identificar_alum_max_nota(promedios)
